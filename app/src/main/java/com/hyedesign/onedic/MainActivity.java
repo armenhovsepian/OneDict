7e8306696999f5c32897ffd8090c606f506212f8
Armen Hovsepian <Armen Hovsepian>
2017-02-16 21:24:17 +0330

    Initial commit

---
+++ app/src/main/java/com/hyedesign/onedic/MainActivity.java
@@ -0,0 +1,17 @@
+package com.hyedesign.onedic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.gson.Gson;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Gson g = new Gson();
+    }
+}
