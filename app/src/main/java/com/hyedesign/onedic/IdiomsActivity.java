1a8170970b3dd6c5680a97bde6a9f8d8a29cf3b0
rmn.hovsepian <rmn.hovsepian@live.com>
2017-02-17 22:31:33 +0330

    Create Main Layout and Corresponding Layouts

---
+++ app/src/main/java/com/hyedesign/onedic/IdiomsActivity.java
@@ -0,0 +1,18 @@
+package com.hyedesign.onedic;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.hyedesign.onedic.R;
+
+/**
+ * Created by Albaloo on 2/17/2017.
+ */
+
+public class IdiomsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_idioms);
+    }
+}
--- app/src/main/java/com/hyedesign/onedic/MainActivity.java
+++ app/src/main/java/com/hyedesign/onedic/MainActivity.java
@@ -1,9 +1,14 @@
 package com.hyedesign.onedic;
 
+import android.content.Intent;
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-import com.google.gson.Gson;
+import com.hyedesign.onedic.utilities.MyTextUtil;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +17,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Gson g = new Gson();
+        MyTextUtil util = new MyTextUtil();
+
+        TextView tv = (TextView) findViewById(R.id.tvTest);
+        tv.setText(util.getValue());
     }
+
+
+    public void redirect_onclick(View view){
+        Intent intent;
+        switch (view.getId()){
+            case R.id.btnIdioms:
+                intent = new Intent(this,IdiomsActivity.class);
+                break;
+
+            case R.id.btnAntithetical:
+                intent = new Intent(this,AntitheticalActivity.class);
+                break;
+
+            case R.id.btnCommon:
+                intent = new Intent(this,CommonActivity.class);
+                break;
+
+            case R.id.btnSynonymous:
+                intent = new Intent(this,SynonymousActivity.class);
+                break;
+
+            case R.id.btnVerbs:
+                intent = new Intent(this,VerbsActivity.class);
+                break;
+
+            case R.id.btnWords:
+                intent = new Intent(this, WordsActivity.class);
+                break;
+
+            default:
+                intent = new Intent(this,MainActivity.class);
+        }
+
+        this.startActivity(intent);
+    }
+
 }
